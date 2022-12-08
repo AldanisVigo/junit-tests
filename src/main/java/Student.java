8138a7252f9e57d8b4fe0619160fd0f96970d76f
Aldanis Vigo <aldanisvigo@gmail.com>
2022-12-08 09:28:43 -0600

    finished bonus

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,2 +1,63 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Student {
+//    The Student class should have a constructor that sets both the name and id property,
+//    it initializes the grades property as an empty ArrayList.
+    private final long id;
+    private String name;
+    private ArrayList<Integer> grades;
+    public Student(long id, String name){
+        this.grades = new ArrayList<>();
+        this.id = id;
+        this.name = name;
+    }
+
+    // returns the student's id
+    public long getId(){
+        return this.id;
+    }
+
+    // returns the student's name
+    public String getName(){
+        return this.name;
+    }
+
+    // adds the given grade to the grades list
+    public void addGrade(int grade){
+        this.grades.add(grade);
+    }
+
+    // returns the list of grades
+    public ArrayList<Integer> getGrades(){
+        return this.grades;
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        try {
+            return (double) this.grades.stream().reduce((a, b) -> a + b).get() / this.grades.size();
+        } catch (NullPointerException e){
+            throw e;
+        }
+    }
+
+    // Go ahead and try to add the rest of the CRUD tests and functionality,
+    // write the methods for updateGrade() and deleteGrade() in the Student class.
+    public void updateGrade(int index, Integer newGrade){
+        try {
+            this.grades.set(index, newGrade);
+        }catch(IndexOutOfBoundsException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void deleteGrade(int index){
+        try {
+            this.grades.remove(index);
+        }catch(IndexOutOfBoundsException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,3 +1,105 @@
-public class StudentTest {
+import org.junit.Before;
+import org.junit.Test;
 
+import java.util.EmptyStackException;
+
+import static org.junit.Assert.*;
+
+public class StudentTest {
+    private Student myStudent;
+    @Before
+    public void setUp(){
+        this.myStudent = new Student(1,"Aldanis Vigo");
+    }
+
+    @Test
+    public void testIsEmpty(){
+        assertTrue(this.myStudent.getGrades().isEmpty());
+    }
+
+
+    @Test
+    public void testSize(){
+        //Since we have no grades starting off, we expect the size to equal 0
+        assertEquals(0, this.myStudent.getGrades().size());
+    }
+
+    @Test
+    public void testAdd(){
+        //We again assert that the array of grades in our student is empty
+        assertTrue(this.myStudent.getGrades().isEmpty());
+
+        // Add a grade to make it not empty anymore
+        this.myStudent.addGrade(80);
+
+        // Making sure the student grades isEmpty returns false once we add an element to it
+        assertFalse(this.myStudent.getGrades().isEmpty());
+    }
+
+    @Test
+    public void testRemove(){
+        //Starting off the array of grades in our student should be empty
+        assertTrue(this.myStudent.getGrades().isEmpty());
+
+        // Add a grade to make it not empty anymore
+        this.myStudent.addGrade(80);
+
+        // Making sure the student grades isEmpty returns false once we add an element to it
+        assertFalse(this.myStudent.getGrades().isEmpty());
+
+        // Remove the item from the grades
+        this.myStudent.deleteGrade(0);
+
+        //Make sure that removing works and the array is empty again
+        assertTrue(this.myStudent.getGrades().isEmpty());
+    }
+
+    @Test
+    public void testContains(){
+        //Check that the student's grades does not contain the value 80
+        assertFalse(myStudent.getGrades().contains(80));
+
+        //Add the value 80 to it
+        this.myStudent.addGrade(80);
+
+        //Check that the student's grades contains 80 now after adding it
+        assertTrue(this.myStudent.getGrades().contains(80));
+    }
+
+    @Test
+    public void testGradesAverage(){
+        //Add a grade of 80
+        this.myStudent.getGrades().add(80);
+        //Add a grade of 70
+        this.myStudent.getGrades().add(70);
+        //Check that average is 75
+        assertTrue(75.0 == this.myStudent.getGradeAverage());
+    }
+
+    @Test
+    public void testName(){
+        //Test that the name is Aldanis Vigo
+        assertEquals("Aldanis Vigo",this.myStudent.getName());
+    }
+
+    @Test
+    public void testId(){
+        //Test that the id is equal to 1
+        assertTrue(this.myStudent.getId() == 1);
+    }
+
+    @Test
+    public void testGradeUpdate(){
+        //Add a grade of 80
+        this.myStudent.addGrade(80);
+
+        //Assert that the value in index 0 is 80
+        assertTrue(this.myStudent.getGrades().get(0) == 80);
+
+        //Update the grade at index 0 to 75
+        this.myStudent.updateGrade(0,75);
+
+        //Assert that the value of index 0 is 75
+        assertTrue(this.myStudent.getGrades().get(0) == 75);
+    }
 }
